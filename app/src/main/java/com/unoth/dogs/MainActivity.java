71064f197d15bc2ed6f4e0d1001c87cad8eb0320
Kristina Dedova <dan005bk@gmail.com>
2023-10-03 20:25:51 +0300

    Refactoring View, added Glide

--- app/src/main/java/com/unoth/dogs/MainActivity.java
+++ app/src/main/java/com/unoth/dogs/MainActivity.java
@@ -1,28 +1,44 @@
 package com.unoth.dogs;
 
 import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
+    private ImageView imageViewDog;
+    private ProgressBar progressBar;
+    private Button buttonLoadImg;
     private MainViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
         viewModel = new ViewModelProvider(this).get(MainViewModel.class);
         viewModel.loadDogImg();
         viewModel.getDogImg().observe(this, new Observer<DogImg>() {
             @Override
             public void onChanged(DogImg dogImg) {
-                Log.d(TAG, dogImg.toString());
+                Glide.with(MainActivity.this)
+                        .load(dogImg.getMessage())
+                        .into(imageViewDog);
             }
         });
+    }
+
+    private void initViews() {
+        imageViewDog = findViewById(R.id.imageViewDog);
+        progressBar = findViewById(R.id.progressBar);
+        buttonLoadImg = findViewById(R.id.buttonLoadImg);
     }
 
 }
